dcf7c5fe04e94d20228e63b530c1ac453e80e10e
vijayparkhe <vijay90p@gmail.com>
2023-03-30 23:42:43 +0530

    minor changes in test file

--- tests/testdemo.py
+++ tests/testdemo.py
@@ -13,7 +13,7 @@ class TestDemoCalculator(unittest.TestCase):
     def test_invalid_addition(self):
         demcal = Democalculator()
         result = demcal.addition(-100,-365)
-        self.assertEqual(result,"Only Positive numbers are allowded")
+        self.assertEqual(result,"1Only Positive numbers are allowded")
         print('test_invalid_addition_ok')
 
     def test_valid_addition(self):
